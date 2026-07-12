501f39084f474874cf5642e567a15035cda96036
Heet-pixel <compushah@gmail.com>
2026-07-13 00:22:22 +0530

    Fixed teacher redirect issue

--- server.js
+++ server.js
@@ -195,7 +195,7 @@ const noCache = {
     }
   },
 };
-app.use('/images',         express.static(path.join(__dirname, 'public', 'images','logo.png')));
+app.use('/teacher', express.static(path.join(__dirname, 'teacher'), noCache));
 app.use('/shared',         express.static(path.join(__dirname, 'shared'),         noCache));
 app.use('/login',          express.static(path.join(__dirname, 'login'),          noCache));
 app.use('/super-admin',    express.static(path.join(__dirname, 'super-admin'),    noCache));
